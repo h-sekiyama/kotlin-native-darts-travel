32b4739689747a6ee9b8ed896eed85b6d4464671
hidemitsu sekiyama <hidemitsu.sekiyama@gmail.com>
2019-10-16 00:41:19 +0900

    わからん・・・

--- ShareCode/src/commonMain/kotlin/common.kt
+++ ShareCode/src/commonMain/kotlin/common.kt
@@ -2,9 +2,6 @@ import com.github.kitwtnb.kotlinnativetutorial.model.api.ApiClient
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import io.ktor.client.request.request
-import io.ktor.http.ContentType.Application.Json
-import model.Results
 import model.Station
 import kotlinx.serialization.*
 import kotlinx.serialization.json.JSON
@@ -19,20 +16,21 @@ fun createApplicationScreenMessage(): String {
 suspend fun sequentialRequests() :String {
     val client = HttpClient()
 
-    val firstBytes = client.get<Station>("https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&start=0")
+    val apiUrl = "https://webservice.recruit.co.jp/manabi/station/v2?prefecture=13&key=3e2b4a21beb30b57&format=json&start=0"
+    val firstBytes = client.get<String>(apiUrl)
 
-    val secondBytes = JSON.stringify(firstBytes);
-    val obj = JSON.parse(Station.serializer(), secondBytes)
+    val apiClient = ApiClient(
+        baseUrl = apiUrl,
+        serializer = KotlinxSerializer(JSON.nonstrict).also {
+            it.setMapper(Station::class, Station.serializer())
+        }
+    )
 
-    client.close()
+    val secondBytes = apiClient.get<String>(apiUrl)
 
-//
-//    val client = OkHttpClient()
-//    val request = Request.Builder()
-//        .url("https://api.cryptowat.ch/markets/coinbase-pro/btcusd/ohlc")
-//        .build()
-//    val response= client.newCall(request).execute()
-//    println(response.body())
+
+    // parsing data back
+    val obj = JSON.parse(Station.serializer(), "firstBytes")
 
     return obj.results.api_version
 
